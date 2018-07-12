67637f997049c18be75a1ba73358e93ab8299422
Keylin_D <keylin.05.01@gmail.com>
2018-07-12 13:06:17 -0400

    conexion con facebook

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,45 @@
+// conexión firebase
+function register(){
+    const emailValue = email.value;
+    const passwordValue = password.value; 
+    const usuarioValue =  usuario.value;
+    firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue,usuarioValue)
+        .then(()=>{
+            console.log("Usuario registrado");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > "+error.code);
+            console.log("Error de firebase, mensaje > "+error.message);
+        });
+  }
+function login(){
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then(()=>{
+            console.log("Usuario con login exitoso");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > "+error.code);
+            console.log("Error de firebase, mensaje > "+error.message);
+        });
+  }
+  function loginFacebook(){
+    const provider = new firebase.auth.FacebookAuthProvider();
+    //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
+    provider.setCustomParameters({
+        'display': 'popup'
+    }); 
+    firebase.auth().signInWithPopup(provider)
+        .then(()=>{
+            console.log("Login con facebook");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > "+error.code);
+            console.log("Error de firebase, mensaje > "+error.message);
+        });
+  }
+
 //Me gusta publicacion
 const heart = document.getElementsByClassName('fa-heart');
 heart.addEventListener('click', ()=> {
