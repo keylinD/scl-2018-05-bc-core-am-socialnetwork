95cf4ffc96c40b5d3e6f56c57eefe5f4bdcdcfd0
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-07-13 08:44:08 -0400

    ordenando funciones

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,50 +1,49 @@
-
 // conexión firebase
 function register(){
-    const emailValue = email.value;
-    const passwordValue = password.value; 
-    // const usuarioValue =  usuario.value;
-    firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-        .then(()=>{
-            console.log("Usuario registrado");
-        })
-        .catch((error)=>{
-            console.log("Error de firebase > "+error.code);
-            console.log("Error de firebase, mensaje > "+error.message);
-        });
+  const emailValue = email.value;
+  const passwordValue = password.value; 
+  // const usuarioValue =  usuario.value;
+  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+    .then(()=>{
+      console.log("Usuario registrado");
+    })
+    .catch((error)=>{
+      console.log("Error de firebase > "+error.code);
+      console.log("Error de firebase, mensaje > "+error.message);
+    });
   }
+
 function login(){
-    const emailValue = email.value;
-    const passwordValue = password.value;
-    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-        .then(()=>{
-            console.log("Usuario con login exitoso");
-        })
-        .catch((error)=>{
-            console.log("Error de firebase > "+error.code);
-            console.log("Error de firebase, mensaje > "+error.message);
-        });
-  }
-  function loginFacebook(){
-    const provider = new firebase.auth.FacebookAuthProvider();
-    //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
-    provider.setCustomParameters({
-        'display': 'popup'
-    }); 
-    firebase.auth().signInWithPopup(provider)
-        .then(()=>{
-            console.log("Login con facebook");
-        })
-        .catch((error)=>{
-            console.log("Error de firebase > "+error.code);
-            console.log("Error de firebase, mensaje > "+error.message);
-        });
-  }
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+  .then(()=>{
+    console.log("Usuario con login exitoso");
+  })
+  .catch((error)=>{
+    console.log("Error de firebase > "+error.code);
+    console.log("Error de firebase, mensaje > "+error.message);
+  });
+}
+
+function loginFacebook(){
+  const provider = new firebase.auth.FacebookAuthProvider();
+  //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
+  provider.setCustomParameters({
+    'display': 'popup'
+  }); 
+  firebase.auth().signInWithPopup(provider)
+  .then(()=>{
+    console.log("Login con facebook");
+  })
+  .catch((error)=>{
+    console.log("Error de firebase > "+error.code);
+    console.log("Error de firebase, mensaje > "+error.message);
+  });
+}
 
 //Me gusta publicacion
-const heart = document.getElementsByClassName('fa-heart');
-
-//Me gusta y contador publicacion
+function publicacion() {
 let contadorPublicacion = [];
 const heart = document.querySelector('i');
 
@@ -56,10 +55,12 @@ heart.addEventListener('click', ()=> {
   }
   return contador.innerHTML = contadorPublicacion;
 })
+}
 
 //Crear nuevo comentario, me gusta, eliminar
-const boton = document.getElementById('btn');
-boton.addEventListener('click', () => {
+function comentario() {
+  const boton = document.getElementById('btn');
+  boton.addEventListener('click', () => {
     let comments = document.getElementById('comment').value;
     document.getElementById('comment').value = '';
     const cont = document.getElementById('cont');
@@ -106,4 +107,5 @@ boton.addEventListener('click', () => {
     newComments.appendChild(trash);
     newComments.appendChild(contenedorElemento);
     cont.appendChild(newComments);
-}) 
+})
+}
