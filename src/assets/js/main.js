b635133be798327d5ebc9a4b40dc9abd49c4e04c
Keylin_D <keylin.05.01@gmail.com>
2018-07-13 08:39:40 -0400

    inicio de sesion con correo y contraseña listo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,19 @@
 // conexión firebase
+window.onload = ()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+        if(user){
+            //Si estamos logueados
+            // loggedOut.style.display = "none";
+            // loggedIn.style.display = "block";
+            console.log("User > "+JSON.stringify(user));
+        }else{
+            //No estamos logueados
+            // loggedOut.style.display = "block";
+            // loggedIn.style.display = "none";
+            console.log('Usuario no logeado');
+        }
+    });
+}
 function register(){
     const emailValue = email.value;
     const passwordValue = password.value; 
@@ -24,6 +39,14 @@ function login(){
             console.log("Error de firebase, mensaje > "+error.message);
         });
   }
+  function logout(){
+    firebase.auth().signOut()
+        .then(()=>{
+            console.log("Chao");
+        })
+        .catch();
+  }
+  
   function loginFacebook(){
     const provider = new firebase.auth.FacebookAuthProvider();
     //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
@@ -40,51 +63,51 @@ function login(){
         });
   }
 
-//Me gusta publicacion
-const heart = document.getElementsByClassName('fa-heart');
-heart.addEventListener('click', ()=> {
-  heart.classList.toggle('red');
-})
+// //Me gusta publicacion
+// const heart = document.getElementsByClassName('fa-heart');
+// heart.addEventListener('click', ()=> {
+//   heart.classList.toggle('red');
+// })
 
-//Crear nuevo comentario, me gusta, eliminar
-const boton = document.getElementById('btn');
-boton.addEventListener('click', () => {
-    let comments = document.getElementById('comment').value;
-    document.getElementById('comment').value = '';
-    const cont = document.getElementById('cont');
-    const newComments = document.createElement('div');
+// //Crear nuevo comentario, me gusta, eliminar
+// const boton = document.getElementById('btn');
+// boton.addEventListener('click', () => {
+//     let comments = document.getElementById('comment').value;
+//     document.getElementById('comment').value = '';
+//     const cont = document.getElementById('cont');
+//     const newComments = document.createElement('div');
 
-    //Para que aparezca si o si comentario
-    if(comments.length === 0 || comments === null){
-      alert ('Debes ingresar un mensaje');
-      return false;
-    }
+//     //Para que aparezca si o si comentario
+//     if(comments.length === 0 || comments === null){
+//       alert ('Debes ingresar un mensaje');
+//       return false;
+//     }
     
-    //corazon
-    const heart = document.createElement('i');
-    heart.classList.add('fa', 'fa-heart', 'heart');
-    //evento click corazon
-    heart.addEventListener('click', ()=> {
-      heart.classList.toggle('red');
-    })
+//     //corazon
+//     const heart = document.createElement('i');
+//     heart.classList.add('fa', 'fa-heart', 'heart');
+//     //evento click corazon
+//     heart.addEventListener('click', ()=> {
+//       heart.classList.toggle('red');
+//     })
     
-    //Basura
-    const trash = document.createElement('i');
-    trash.classList.add('fa', 'fa-trash', 'trash');
-    //Evento click basura
-    trash.addEventListener('click', ()=> {
-        let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
-      if (confirmarEliminar == true) {
-        cont.removeChild(newComments);
-      }
-    })
+//     //Basura
+//     const trash = document.createElement('i');
+//     trash.classList.add('fa', 'fa-trash', 'trash');
+//     //Evento click basura
+//     trash.addEventListener('click', ()=> {
+//         let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
+//       if (confirmarEliminar == true) {
+//         cont.removeChild(newComments);
+//       }
+//     })
 
-    //Crear p nuevo con comentario
-    const contenedorElemento = document.createElement('p');
-    let textNewComment = document.createTextNode(comments);
-    contenedorElemento.appendChild(textNewComment);
-    newComments.appendChild(heart);
-    newComments.appendChild(trash);
-    newComments.appendChild(contenedorElemento);
-    cont.appendChild(newComments);
-}) 
+//     //Crear p nuevo con comentario
+//     const contenedorElemento = document.createElement('p');
+//     let textNewComment = document.createTextNode(comments);
+//     contenedorElemento.appendChild(textNewComment);
+//     newComments.appendChild(heart);
+//     newComments.appendChild(trash);
+//     newComments.appendChild(contenedorElemento);
+//     cont.appendChild(newComments);
+// }) 
