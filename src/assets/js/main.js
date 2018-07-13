2ddbe92d52bdf9bc1f6025c70969a40c41a2633d
Keylin_D <keylin.05.01@gmail.com>
2018-07-13 09:30:20 -0400

    cambios

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -64,29 +64,21 @@ function login(){
         });
   }
 
-<<<<<<< HEAD
 // //Me gusta publicacion
 // const heart = document.getElementsByClassName('fa-heart');
+
+// //Me gusta y contador publicacion
+// let contadorPublicacion = [];
+// const heart = document.querySelector('i');
+
 // heart.addEventListener('click', ()=> {
-//   heart.classList.toggle('red');
+//   if (heart.classList.toggle('red')){
+//     contadorPublicacion++;
+//   }else{
+//     contadorPublicacion--;
+//   }
+//   return contador.innerHTML = contadorPublicacion;
 // })
-=======
-//Me gusta publicacion
-const heart = document.getElementsByClassName('fa-heart');
-
-//Me gusta y contador publicacion
-let contadorPublicacion = [];
-const heart = document.querySelector('i');
-
-heart.addEventListener('click', ()=> {
-  if (heart.classList.toggle('red')){
-    contadorPublicacion++;
-  }else{
-    contadorPublicacion--;
-  }
-  return contador.innerHTML = contadorPublicacion;
-})
->>>>>>> 66704d89f1beadda08fe62229866336497e971b7
 
 // //Crear nuevo comentario, me gusta, eliminar
 // const boton = document.getElementById('btn');
@@ -102,49 +94,39 @@ heart.addEventListener('click', ()=> {
 //       return false;
 //     }
     
-<<<<<<< HEAD
 //     //corazon
 //     const heart = document.createElement('i');
+//     const contadorheart = document.createElement('span')
+//   //falta contador comentario!!!!!---------
 //     heart.classList.add('fa', 'fa-heart', 'heart');
 //     //evento click corazon
+//     let contadorPublicacion = [];
 //     heart.addEventListener('click', ()=> {
-//       heart.classList.toggle('red');
-//     })
-=======
-    //corazon
-    const heart = document.createElement('i');
-    const contadorheart = document.createElement('span')
-  //falta contador comentario!!!!!---------
-    heart.classList.add('fa', 'fa-heart', 'heart');
-    //evento click corazon
-    let contadorPublicacion = [];
-    heart.addEventListener('click', ()=> {
-      if (heart.classList.toggle('red')){
-        contadorComentario++;
-      }else{
-        contadorComentario--;
-      }
-      return contadorheart.innerHTML = contadorComentario;
-    })
->>>>>>> 66704d89f1beadda08fe62229866336497e971b7
-    
-//     //Basura
-//     const trash = document.createElement('i');
-//     trash.classList.add('fa', 'fa-trash', 'trash');
-//     //Evento click basura
-//     trash.addEventListener('click', ()=> {
-//         let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
-//       if (confirmarEliminar == true) {
-//         cont.removeChild(newComments);
+//       if (heart.classList.toggle('red')){
+//         contadorComentario++;
+//       }else{
+//         contadorComentario--;
 //       }
+//       return contadorheart.innerHTML = contadorComentario;
 //     })
+    
+// //     //Basura
+// //     const trash = document.createElement('i');
+// //     trash.classList.add('fa', 'fa-trash', 'trash');
+// //     //Evento click basura
+// //     trash.addEventListener('click', ()=> {
+// //         let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
+// //       if (confirmarEliminar == true) {
+// //         cont.removeChild(newComments);
+// //       }
+// //     })
 
-//     //Crear p nuevo con comentario
-//     const contenedorElemento = document.createElement('p');
-//     let textNewComment = document.createTextNode(comments);
-//     contenedorElemento.appendChild(textNewComment);
-//     newComments.appendChild(heart);
-//     newComments.appendChild(trash);
-//     newComments.appendChild(contenedorElemento);
-//     cont.appendChild(newComments);
-// }) 
+// //     //Crear p nuevo con comentario
+// //     const contenedorElemento = document.createElement('p');
+// //     let textNewComment = document.createTextNode(comments);
+// //     contenedorElemento.appendChild(textNewComment);
+// //     newComments.appendChild(heart);
+// //     newComments.appendChild(trash);
+// //     newComments.appendChild(contenedorElemento);
+// //     cont.appendChild(newComments);
+// // }) 
