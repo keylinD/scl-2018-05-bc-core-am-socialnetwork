05bc39a8e874409b8a598e0eb321719c821529af
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-07-19 09:03:22 -0400

    cambiando nombre de html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,70 +1,159 @@
-//Crear nuevo comentario, me gusta, eliminar
+firebase.initializeApp({
+  apiKey: "AIzaSyAsyUFU3YRE0ZFQBsX06RIr0jkZIwNDZrI",
+  authDomain: "foodgram-65316.firebaseapp.com",
+  databaseURL: "https://foodgram-65316.firebaseio.com",
+  projectId: "foodgram-65316",
+  storageBucket: "foodgram-65316.appspot.com",
+  messagingSenderId: "186832450814"
+});
 
-function comentar() {
-  let comments = document.getElementById('comment').value;
-  document.getElementById('comment').value = '';
-  const cont = document.getElementById('cont');
-  const newComments = document.createElement('div');
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
 
-  //Para que aparezca si o si comentario
-  if(comments.length === 0 || comments === null){
-    alert ('Debes ingresar un mensaje');
-    return false;
-  }
+function guardar(){
+  const custom = customFile.files[0];
+  const fileName = custom.name;
+  const metadata = {
+    contentType: custom.type
+  };
+  const task = firebase.storage().ref('images') 
+      .child(fileName)
+      .put(custom, metadata);
+
+  task.then(snapshot => snapshot.ref.getDownloadURL())  //obtenemos la url de descarga (de la imagen)
+    .then(url => {
+        console.log("URL del archivo > "+url);
+        const titulopublicacion = document.getElementById('titulopublicacion').value;
+        document.getElementById('titulopublicacion').value = '';
+        const publicacion = document.getElementById('publicacion').value;
+        document.getElementById('publicacion').value = '';
   
-  //corazon
-  const heart = document.createElement('i');
-  const contadorheart = document.createElement('span');
-  heart.appendChild(contadorheart);
-  heart.classList.add('fa', 'fa-heart', 'heart');
-  //evento click corazon
-  let contadorComentario = [];
-  heart.addEventListener('click', ()=> {
-    if (heart.classList.toggle('red')){
-      contadorComentario++;
-    }else{
-      contadorComentario--;
-    }
-    return contadorheart.innerHTML = contadorComentario;
-  })
+        db.collection("publicacion").add({  
+          title: titulopublicacion,
+          text: publicacion,
+          img: url,
+        })
+        .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+          console.error("Error adding document: ", error);
+      });
+      });
+}
 
-  //Editar
-  const edit = document.createElement('i');
-  edit.classList.add('fas', 'fa-pencil-alt');
-  //Evento click editar
-  edit.addEventListener('click', ()=> {
-    contenedorElemento.contentEditable = true;
-    contenedorElemento.addEventListener('keydown', (event)=> {
-      if (event.which == 13){
-        let confirmarEditar = confirm('¿Estas seguro que quieres modificar tu comentario?');
-        if (confirmarEditar == true) {
-          contenedorElemento.removeAttribute('contentEditable');
-        } else {
+//leer documentos
+let card = document.getElementById('cardPublicacion');
 
-        }
-      }
+db.collection("publicacion").onSnapshot((querySnapshot) => {
+  card.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().img}`);
+      card.innerHTML += `
+      <div class="card">
+      <img class="card-img-top" src="${doc.data().img}"text=Image cap" alt="Card image cap">
+        <h5 class="card-title">${doc.data().title}</h5>
+        <p class="card-text">${doc.data().text}</p>
+        <i class="fas fa-trash-alt" onclick="eliminar('${doc.id}')"></i>
+        <i class="fas fa-pencil-alt" onclick="editar('${doc.id}', '${doc.data().title}', '${doc.data().text}')"></i>
+
+      </div>
+      <section class="center">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <textarea class="txt" id="comment" placeholder="Añade un comentario..."></textarea>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <button type="submit" class="btn" id="btncomentario" onclick="comentar()">
+                <i class="fas fa-plus" aria-hidden="true"></i> Comentar</button>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12" id="cont"></div>
+          </div>
+        </div>
+      </section>
+      `
+  });
+});
+
+//Editar publicacion
+function editar(id, titulopublicacion, publicacion) {
+
+  document.getElementById('titulopublicacion').value = titulopublicacion;
+  document.getElementById('publicacion').value = publicacion;
+  const boton = document.getElementById('btnPublicar');
+  boton.innerHTML = 'Editar';
+  alert('Sube para editar tu publicación');
+
+
+  boton.onclick = function() {
+    var washingtonRef = db.collection("publicacion").doc(id);
+    // Set the "capital" field of the city 'DC'
+    const titulopublicacion = document.getElementById('titulopublicacion').value;
+    const publicacion = document.getElementById('publicacion').value;
+
+    return washingtonRef.update({
+      title: titulopublicacion,
+      text: publicacion,
+
     })
-  })
-  
-  //Basura
-  const trash = document.createElement('i');
-  trash.classList.add('fa', 'fa-trash', 'trash');
-  //Evento click basura
-  trash.addEventListener('click', ()=> {
-      let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
-    if (confirmarEliminar == true) {
-      cont.removeChild(newComments);
-    }
-  })
+    .then(function() {
+        console.log("Document successfully updated!");
+        boton.innerHTML = 'Guardar';
+        document.getElementById('titulopublicacion').value = '';
+        document.getElementById('publicacion').value = '';
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+      }
+}
+/*
+function editar(id, titulopublicacion, publicacion, url) {
 
-  //Crear p nuevo con comentario
-  const contenedorElemento = document.createElement('p');
-  let textNewComment = document.createTextNode(comments);
-  contenedorElemento.appendChild(textNewComment);
-  newComments.appendChild(heart);
-  newComments.appendChild(edit);
-  newComments.appendChild(trash);
-  newComments.appendChild(contenedorElemento);
-  cont.appendChild(newComments);
+  document.getElementById('titulopublicacion').value = titulopublicacion;
+  document.getElementById('publicacion').value = publicacion;
+  document.getElementById('url').value = url;
+
+let editarRef = db.collection("publicacion").doc(id);
+
+let boton = document.getElementById('btnEditar');
+
+let titulopublicacion = document.getElementById('titulopublicacion').value;
+let publicacion = document.getElementById('publicacion').value;
+let url = document.getElementById('url').value;
+
+// Set the "capital" field of the city 'DC'
+return editarRef.update({
+  title: titulopublicacion,
+  text: publicacion,
+  img: url
+})
+.then(function() {
+    console.log("Document successfully updated!");
+    boton.innerHTML = "Guardar";
+})
+.catch(function(error) {
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
+});
 
 }
+*/
+
+function eliminar(id) {
+  let confirmarEliminar = confirm('¿Estas seguro de eliminar?');
+  if (confirmarEliminar == true) {
+    db.collection("publicacion").doc(id).delete().then(function() {
+    console.log("Document successfully deleted!");
+    }).catch(function(error) {
+    console.error("Error removing document: ", error);
+    });
+  }
+}
+
